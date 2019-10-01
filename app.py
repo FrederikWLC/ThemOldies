275d156ae420bf6dc8b5dd14cab49d866cf42ab4
Frederik Walther Liberoth Christoffersen <frederik210103@gmail.com>
2019-10-01 22:59:54 +0200

    Added some cool and juicy errors

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import stripe
 from scripts import tabledef
 from scripts import forms
 from scripts import helpers
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 import json
 import sys
 import os
 from settings import stripe_plan_id, stripe_public_key, stripe_secret_key
 from flask_dropzone import Dropzone
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+
+
 app = Flask(__name__)
+app.config['TESTING'] = False
+app.config["LOGIN_DISABLED"] = False
+login = LoginManager()
+login.init_app(app)
+login.login_view = "login"
 dropzone = Dropzone(app)
 app.secret_key = os.urandom(12)  # Generic key for dev purposes only
 stripe.api_key = stripe_secret_key
@@ -21,9 +29,10 @@ stripe.api_key = stripe_secret_key
 
 # ======== Routing =========================================================== #
 # -------- Login ------------------------------------------------------------- #
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if not session.get('logged_in'):
+    if not current_user:
         form = forms.LoginForm(request.form)
         if request.method == 'POST':
             username = request.form['username'].lower()
@@ -32,17 +41,25 @@ def login():
                 if helpers.credentials_valid(username, password):
                     session['logged_in'] = True
                     session['username'] = username
-                    return json.dumps({'status': 'Login successful'})
-                return json.dumps({'status': 'Invalid user/pass'})
-            return json.dumps({'status': 'Both fields required'})
+                    user = helper.get_user()
+                    login(user, remember=True)
+                    return redirect(url_for("home"))
+                flash("Invalid username or password")
+                print("Invalid username or password")
+                return redirect(url_for("login"))
+            flash("Both fields required")
+            print("Both fields required")
+            return redirect(url_for("login"))
         return render_template('login.html', form=form)
     user = helpers.get_user()
     return render_template('home.html', user=user)
 
 
 @app.route("/logout")
+@login_required
 def logout():
     session['logged_in'] = False
+    logout_user()
     return redirect(url_for('login'))
 
 
@@ -60,15 +77,20 @@ def signup():
                     helpers.add_user(username, password, email)
                     session['logged_in'] = True
                     session['username'] = username
-                    return json.dumps({'status': 'Signup successful'})
-                return json.dumps({'status': 'Username taken'})
-            return json.dumps({'status': 'User/Pass required'})
+                    flash("Signup was succesful")
+                    return redirect(url_for("home"))
+                flash("Username taken")
+                return redirect(url_for("signup"))
+            flash("Username and password required")
+            return redirect(url_for("signup"))
         return render_template('login.html', form=form)
     return redirect(url_for('login'))
 
 
 # -------- Settings Page ---------------------------------------------------------- #
+
 @app.route('/settings', methods=['GET', 'POST'])
+@login_required
 def settings():
     if session.get('logged_in'):
         if request.method == 'POST':
@@ -84,16 +106,12 @@ def settings():
 
 
 # -------- Home page ---------------------------------------------------------- #
+
 @app.route("/")
 @app.route("/index")
 @app.route("/main")
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    if session.get('logged_in'):
-        user = helpers.get_user()
-        if user.subscription:
-            return redirect(url_for('upload'))
-        return render_template("home.html", user=user)
     return render_template("home.html")
 
 # -------- Subscribe page (stripe)---------------------------------------------------------- #
@@ -101,57 +119,61 @@ def home():
 
 @app.route("/sub")
 @app.route("/subscribe", methods=['GET', 'POST'])
+@login_required
 def subscribe():
-    if session.get('logged_in'):
-        buy = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            subscription_data={
-                'items': [{
-                    'plan': stripe_plan_id,
-                }],
-            },
-            success_url='http://localhost:5000/success',
-            cancel_url='http://localhost:5000/cancel',
-        )
 
-        print(buy)
-        print("+" * 42)
-        _id = buy.get("id")
-        print(_id)
-        user = helpers.get_user()
-        user.subscription = True
-        return render_template('subscribe.html', _id=_id, stripe_public_key=stripe_public_key, user=user)
-    return redirect(url_for('login'))
+    buy = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        subscription_data={
+            'items': [{
+                'plan': stripe_plan_id,
+            }],
+        },
+        success_url='http://localhost:5000/success',
+        cancel_url='http://localhost:5000/cancel',
+    )
+
+    print(buy)
+    print("+" * 42)
+    _id = buy.get("id")
+    print(_id)
+    user = helpers.get_user()
+    return render_template('subscribe.html', _id=_id, stripe_public_key=stripe_public_key, user=user)
 # -------- Subscription Succes Page ---------------------------------------------------------- #
 
 
 @app.route('/success')
+@login_required
 def success():
+    user = helpers.get_user()
+    user.subscription = True
     return render_template('success.html')
 
 # -------- Subscription Cancel page ---------------------------------------------------------- #
 
 
 @app.route('/cancel')
+@login_required
 def cancel():
     return "CANCELED!"
 
+
 #---------- Dropzone -------------------------------------------------------------------------
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     if session.get('logged_in'):
         user = helpers.get_user()
-        if user.subscription: 
+        if user.subscription:
             if request.method == 'POST':
                 for key, f in request.files.items():
                     if key.startswith('file'):
-                        #Handling of .csv file
+                        # Handling of .csv file
                         pass
             return render_template('upload.html')
         return redirect(url_for("subscribe"))
     return redirect(url_for("login"))
 
-    
 
 
 # ======== Main ============================================================== #
