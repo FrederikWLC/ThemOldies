39d50f6874b8082413fccf994accaa81cd2c1cd3
Frederik Walther Liberoth Christoffersen <frederik210103@gmail.com>
2019-10-13 21:41:15 +0200

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# midterm
+# ThemOldies
+
+To run the program, first install the packages by typing this line into your console with the current directory being the one that contains this application.
+´´´
+pip install -r requirement.txt
+´´´
+Then type in this command
+´´´
+flask run --without-threads
+´´´
+You can then register, subscribe and upload a csv file including addresses in a collumn called, "Address". And you will be able to download an updated version of that file including price predictions of the property on these addresses.
+
+As an example csv file, you can make use of the example provided in this repository, called "example_input.csv".
+
+*This app is not yet complete because of errors in terms of communication between client and server and communication between server and the google streetview api. Therefore all it does for now is providing the user with price predictions of 'N/A'.
--- app/config.py
+++ app/config.py
@@ -9,11 +9,11 @@ basedir = path.abspath(path.dirname(__file__))
 class Config(object):
     SECRET_KEY = getenv("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(basedir, "app.db")
-    STRIPE_PUBLIC_KEY = getenv("STRIPE_PUBLIC_KEY")
-    STRIPE_SECRET_KEY = getenv("STRIPE_SECRET_KEY")
-    STRIPE_PLAN_ID = getenv("STRIPE_PLAN_ID")
+    STRIPE_PUBLIC_KEY = getenv("STRIPE_PUBLIC_KEY") or "pk_test_hMThRnXpDt7ZLMZoWtPoJ00o00CYyU8PxR"
+    STRIPE_SECRET_KEY = getenv("STRIPE_SECRET_KEY") or "sk_test_HDn2ehB7Qr2AMNVIsjYtDPdq003xHUelvf"
+    STRIPE_PLAN_ID = getenv("STRIPE_PLAN_ID") or "plan_FskyQzupz1I56F"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DROPZONE_ALLOWED_FILE_CUSTOM = True
     DROPZONE_ALLOWED_FILE_TYPE = '.csv'
     PERMANENT_SESSION_LIFETIME = True
-    STREETVIEW_KEY = getenv("STREETVIEW_KEY")
+    STREETVIEW_KEY = getenv("STREETVIEW_KEY") or 'AIzaSyD_IdPLoeb1zR4VLiNClTBT1nq-U9JiQl8'
