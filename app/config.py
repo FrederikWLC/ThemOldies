5b8bc2e1d94d5bc2adf1520fe37dcd1da368d5f8
Frederik Walther Liberoth Christoffersen <47148276+FrederikWLC@users.noreply.github.com>
2019-11-03 20:18:43 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,8 +9,8 @@ basedir = path.abspath(path.dirname(__file__))
 class Config(object):
     SECRET_KEY = getenv("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(basedir, "app.db")
-    STRIPE_PUBLIC_KEY = getenv("STRIPE_PUBLIC_KEY") or "pk_test_hMThRnXpDt7ZLMZoWtPoJ00o00CYyU8PxR"
-    STRIPE_SECRET_KEY = getenv("STRIPE_SECRET_KEY") or "sk_test_HDn2ehB7Qr2AMNVIsjYtDPdq003xHUelvf"
+    STRIPE_PUBLIC_KEY = getenv("STRIPE_PUBLIC_KEY") or "PUBLIC_KEY"
+    STRIPE_SECRET_KEY = getenv("STRIPE_SECRET_KEY") or "SECRET_KEY"
     STRIPE_PLAN_ID = getenv("STRIPE_PLAN_ID") or "plan_FskyQzupz1I56F"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DROPZONE_ALLOWED_FILE_CUSTOM = True
